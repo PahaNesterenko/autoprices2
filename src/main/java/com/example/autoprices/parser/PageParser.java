e838eae358cc8344d13638274fa9a640559f5cda
PahaNesterenko <karandash5410@gmail.com>
2017-05-15 23:45:26 +0300

    some changes

--- src/main/java/com/example/autoprices/parser/PageParser.java
+++ src/main/java/com/example/autoprices/parser/PageParser.java
@@ -2,20 +2,24 @@ package com.example.autoprices.parser;
 
 import com.example.autoprices.Selectors;
 import com.example.autoprices.domain.Advert;
-import com.example.autoprices.domain.Auto;
+import lombok.SneakyThrows;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class PageParser {
 
     private AutoParser autoParser;
     private AdvertParser advertParser;
 
+    @SneakyThrows
     public List<Advert> parse(Document doc) {
         autoParser = new AutoParser();
         advertParser = new AdvertParser();
@@ -25,15 +29,16 @@ public class PageParser {
         Elements select = doc.select(Selectors.script);
 
 
-        Elements script = doc.select("script");
         int count = 0;
         StringBuilder sb = new StringBuilder();
-        for(Element e : script) {
+        for(Element e : select) {
 
             List<Node> nodes = e.childNodes();
             for (Node node : nodes){
                 String nodeText = node.toString().replace("\n", "");
-                sb.append(nodeText);
+                if(nodeText.contains("window.ria.server.results")) {
+                    sb.append(nodeText);
+                }
             }
 
             /*if( e.childNode(0).attributes().get("data").contains("window.ria.server.results")){
@@ -44,15 +49,26 @@ public class PageParser {
 
         String s = sb.toString();
 
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+        Object eval = engine.eval("function foo(){ var window={};" + s + " return window.ria.server.results;} foo();");
+
+        Map<Long, Object> map = (Map<Long, Object>) eval;
+
+
+
+       /* Gson gson = new GsonBuilder().create();
+        ResultType p = gson.fromJson( eval, ResultType.class);
+        System.out.println(p);*/
+
 
         List<Advert> adverts = new ArrayList<>();
-        for (Element e : select) {
+       /* for (Element e : select) {
 
             Auto auto = autoParser.parse(e);
             Advert advert = advertParser.parse(e);
             advert.setAuto(auto);
             adverts.add(advert);
-        }
+        }*/
 
         return adverts;
 
